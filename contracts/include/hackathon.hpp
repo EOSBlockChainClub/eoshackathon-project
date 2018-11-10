2d3250b99c7db882a8c249aaaff8164b3a206eda
craigbranscom <craig.branscom@gmail.com>
2018-11-10 14:11:33 -0800

    initial spec

--- contracts/include/hackathon.hpp
+++ contracts/include/hackathon.hpp
@@ -5,3 +5,56 @@
  * @author Craig Branscom
  */
 
+#include <eosiolib/eosio.hpp>
+
+using namespace eosio;
+using namespace std;
+
+class [[eosio::contract]] test : public contract {
+
+public:
+
+    using contract::contract;
+
+    //test() : contract();
+
+    struct [[eosio::table]] artist_info {
+        name artist;
+        //uint16_t published_songs;
+        
+        uint64_t primary_key() const { return artist.value; }
+        EOSLIB_SERIALIZE(artist_info, (artist)))
+    }
+
+    struct [[eosio:table]] listener_info {
+        name listener;
+        //uint64_t last_song_played;
+        //uint32_t last_play_time;
+
+        uint64_t primary_key() const { return listener.value; }
+        EOSLIB_SERIALIZE(listener_info, (listener)) 
+    }
+
+    struct [[eosio::table]] song {
+        uint64_t song_id;
+        name artist;
+        string ipfs_link;
+
+        uint64_t primary_key() const { return song_id; }
+        //TODO: add secondary index by artist?
+        EOSLIB_SERIALIZE(song, (song_id)(artist)(ipfs_link))
+    };
+
+    typedef multi_index<name("artists"), artist_info> artists_table;
+
+    typedef multi_index<name("listeners"), listener_info> listeners_table;
+
+    typedef multi_index<name("songs"), song> songs_table; 
+
+    [[eosio::action]]
+    void postsong(name artist, string ipfs_link);
+
+    [[eosio::action]]
+    void streamsong(uint64_t song_id, name listener);
+
+};
--- contracts/src/hackathon.cpp
+++ contracts/src/hackathon.cpp
@@ -5,3 +5,46 @@
  * @author Craig Branscom
  */
 
+#include <hackathon.hpp>
+
+class [[eosio::contract]] test : public contract {
+
+public:
+
+    uint64_t test;
+
+    void postsong(name artist, string ipfs_link){
+        require_auth(artist);
+
+        //TODO: check for existing ipfs_link?
+
+        songs_table songs(_self, _self.value);
+        auto new_song_id = songs.available_primary_key();
+
+        artists_table artists(_self, _self.value);
+        auto art_itr = artists.find(artist.value);
+        //eosio_assert(art_itr != artists.end(), "artist doesn't exist on the platform");
+
+        statstable.emplace(_self, [&]( auto& l ) { //NOTE: payer may need to change
+            l.song_id = new_song_id;
+            l.artist = artist;
+            l.ipfs_link = ipfs_link;
+        });
+
+    }
+
+    void streamsong(uint64_t song_id, name listener) {
+        require_auth(listener);
+
+        songs_table songs(_self, _self.value);
+        auto s_itr = songs.find(song_id);
+        //eosio_assert(s_itr != songs.end(), "song doesn't exist on the platform");
+
+        //TODO: pay tokens for listening
+        //TODO: don't award for listening to own music
+
+    }
+
+};
+
+EOSIO_DISPATCH(test, (postsong)(streamsong))
