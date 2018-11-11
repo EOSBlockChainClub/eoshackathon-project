4fd20283888dd4d0988d88f4ec29e975ad53b460
ARoomWithABue <peter@hornofthemoon.com>
2018-11-11 09:33:28 -0800

    mostly finished interface class, still working on mock data

--- web/src/MusicInterface.js
+++ web/src/MusicInterface.js
@@ -1,4 +1,4 @@
-const {Api, Rpc, SignatureProvider} = require('eosjs');
+const {Api, JsonRpc, JsSignatureProvider} = require('eosjs');
 const fetch = require('node-fetch');
 const {TextDecoder, TextEncoder} = require('text-encoding');
 
@@ -8,10 +8,53 @@ class MusicInterface {
         if (!Array.isArray(keys)) {
             keys = [keys];
         }
-        let signatureProvider = new SignatureProvider(keys);
-        let rpc = new Rpc.JsonRpc(rpcConnectionString, {fetch});
+        let signatureProvider = new JsSignatureProvider(keys);
+        let rpc = new JsonRpc(rpcConnectionString, {fetch});
         this.contractName = contractName;
         this.api = new Api({rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder()});
+    }
+
+    async initializeMockData() {
+        let userAccounts = [
+            "useraaaaaaaa",
+            "useraaaaaaab",
+            "useraaaaaaac",
+            "useraaaaaaad",
+            "useraaaaaaae",
+            "useraaaaaaaf",
+            "useraaaaaaag"
+        ];
+
+
+        //ADD listeners
+        for (let i = 0; i < 3; i++) {
+            await this.regListener(userAccounts[i]);
+        }
+
+        //add artists/albums/song
+        await this.regArtist(userAccounts[3], "Queen");
+
+        await this.postAlbum(userAccounts[3], "Jazz");
+        let id = await this.getLastAlbumId(userAccounts[3]);
+        await this.addSong(userAccounts[3], "Don't Stop Me Now", id, "Qmadjkfhadlkfhalsdkfj");
+
+        await this.postAlbum(userAccounts[3], "News of the World");
+        id = await this.getLastAlbumId(userAccounts[3]);
+        await this.addSong(userAccounts[3], "Bohemian Rhapsody", id, "Qmklsdfjasdhfjksadhflkhasd");
+
+        await this.regArtist(userAccounts[4], "Modest Mouse");
+        await this.postAlbum(userAccounts[4], "Good News for People Who Love Bad News");
+        id = await this.getLastAlbumId(userAccounts[4]);
+        await this.addSongs(userAccounts[4], id, [
+            { songName: "Float On", link: "Qmadsfjkhadsljkfhasljdkfh"},
+            { songName: "Ocean Breathes Salty", link: "Qmadjkfhlakdhfkljshadf"},
+            { songName: "The World at Large", link: "Qmdsakjfhaskdhflkasdjhfl"}
+        ]);
+
+        await this.regArtist(userAccounts[5], "Bag Raiders");
+        await this.postAlbum(userAccounts[5], "Bag Raiders");
+        id = await this.getLastAlbumId(userAccounts[5]);
+        await this.addSong(userAccounts[5], "Shooting Stars", id, "Qmfdajskhvjhbuyaef");
     }
 
     //TODO: configuration actions and finalization actions
@@ -21,11 +64,11 @@ class MusicInterface {
         let action = {
             account: this.contractName,
             name: 'reglistener',
-            authorization: {actor: listenerName, permission: 'active'},
+            authorization: [{actor: listenerName, permission: 'active'}],
             data: {member: listenerName}
-        }
+        };
 
-        return await this.send(action);
+        return await this.send([action]);
     }
 
     //TODO: getListener
@@ -40,15 +83,15 @@ class MusicInterface {
     }
 
     //TODO: regArtist args: name
-    async regArtist(artistName) {
+    async regArtist(artistName, groupName) {
         let action = {
             account: this.contractName,
             name: 'regartist',
-            authorization: {actor: artistName, permission: 'active'},
-            data: {member: artistName}
-        }
+            authorization: [{actor: artistName, permission: 'active'}],
+            data: {member: artistName, band_name: groupName}
+        };
 
-        return await this.send(action);
+        return await this.send([action]);
     }
 
     //TODO: getArtist
@@ -67,11 +110,11 @@ class MusicInterface {
         let action = {
             account: this.contractName,
             name: 'postalbum',
-            authorization: {actor: artistName, permission: 'active'},
+            authorization: [{actor: artistName, permission: 'active'}],
             data: {artist: artistName, album_name: albumName}
-        }
+        };
 
-        return await this.send(action);
+        return await this.send([action]);
     }
 
     //TODO: getAlbums
@@ -83,7 +126,7 @@ class MusicInterface {
     //TODO: getLastAlbumId
     async getLastAlbumId(artistName) {
         let albums = await this.getAlbums(artistName);
-        return albums[albums.length - 1].artist;
+        return await albums[albums.length - 1].album_id;
     }
 
     //TODO: getAlbum
@@ -93,29 +136,29 @@ class MusicInterface {
             scope: artistName,
             table: "albums",
             table_key: id
-        })
-        return response['row'][0];
+        });
+        return await response['rows'][0];
     }
 
     //TODO: addSong args: name artist, uin64_t album_id, string ipfs_link
     async addSong(artistName, songName, albumId, link) {
         let action = this.createSongAction(artistName, songName, albumId, link);
 
-        return await this.send(action);
+        return await this.send([action]);
     }
 
     createSongAction(artistName, songName, albumId, link) {
         return {
             account: this.contractName,
             name: 'addsong',
-            authorization: {actor: artistName, permission: 'active'},
+            authorization: [{actor: artistName, permission: 'active'}],
             data: {artist: artistName, song_name: songName, album_id: albumId, ipfs_link: link}
         };
     }
 
     //TODO: BULK addsong
     async addSongs(artistName, albumId, songs) {
-        let actions = []
+        let actions = [];
         songs.forEach((song) => {
             actions.push(this.createSongAction(artistName, song.songName, albumId, song.link))
         });
@@ -124,8 +167,20 @@ class MusicInterface {
 
     //TODO: getSongsFromAlbum
     async getSongsFromAlbum(albumId) {
-        let respone = this.api.rpc.get_table_rows({code: this.contractName, scope: albumId, table: "songs"})
-        return respone['rows'];
+        let response = await this.api.rpc.get_table_rows({code: this.contractName, scope: albumId, table: "songs"});
+        return response['rows'];
+    }
+
+    async getAllSongsFromArtist(artistName){
+        let albums = await this.getAlbums(artistName);
+        let album_songs = [];
+
+        albums.forEach(async (album) => {
+           let albumsSongs = await this.getSongsFromAlbum(album.album_id);
+           console.log(albumsSongs);
+           album_songs.push({ album: album, songs: albumsSongs });
+        });
+        return album_songs;
     }
 
     //TODO: streamSong args: uint64_t song_id, name listener
@@ -135,8 +190,8 @@ class MusicInterface {
             name: "streamsong",
             authorization: {actor: listener, permission: "active"},
             data: {song_id: songId, listener: listener}
-        }
-        return await this.send(action);
+        };
+        return await this.send([action]);
     }
 
     //TODO: vote changing...
@@ -149,10 +204,10 @@ class MusicInterface {
         let action = {
             account: this.contractName,
             name: "subscribe",
-            authorization: {actor: listener, permission: "active"},
+            authorization: [{actor: listener, permission: "active"}],
             data: {listener: listener}
-        }
-        return await this.send(action);
+        };
+        return await this.send([action]);
     }
 
     async send(actions) {
@@ -161,5 +216,39 @@ class MusicInterface {
         }, {blocksBehind: 3, expireSeconds: 30});
     }
 }
+(async function (){
 
-module.exports = {MusicInterface};
+    let keys = [
+        "5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5",
+        "5KLqT1UFxVnKRWkjvhFur4sECrPhciuUqsYRihc1p9rxhXQMZBg",
+        "5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7",
+        "5KNm1BgaopP9n5NqJDo9rbr49zJFWJTMJheLoLM5b7gjdhqAwCx",
+        "5KE2UNPCZX5QepKcLpLXVCLdAw7dBfJFJnuCHhXUf61hPRMtUZg",
+        "5KaqYiQzKsXXXxVvrG8Q3ECZdQAj2hNcvCgGEubRvvq7CU3LySK",
+        "5KFyaxQW8L6uXFB6wSgC44EsAbzC7ideyhhQ68tiYfdKQp69xKo"
+    ];
+
+    let userAccounts = [
+        "useraaaaaaaa",
+        "useraaaaaaab",
+        "useraaaaaaac",
+        "useraaaaaaad",
+        "useraaaaaaae",
+        "useraaaaaaaf",
+        "useraaaaaaag"
+    ];
+
+    let contractName = "useraaaaaaag";
+    let interface = new MusicInterface(keys, contractName, "http://64.38.145.139:8888");
+    // try {
+    //     await interface.initializeMockData();
+    // } catch (e) {
+    //     console.log("Error initializing mock data: ", e);
+    // }
+
+    let artist = await interface.getArtist(userAccounts[3]);
+    let album_songs = await interface.getAllSongsFromArtist(userAccounts[3]);
+
+    console.log(artist);
+    console.log(album_songs);
+})();
