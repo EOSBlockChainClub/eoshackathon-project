1354f5da041a99556dc8407edd4a0e49bddc60ec
ARoomWithABue <peter@hornofthemoon.com>
2018-11-11 09:34:59 -0800

    return music interfaces

--- web/src/MusicInterface.js
+++ web/src/MusicInterface.js
@@ -216,39 +216,45 @@ class MusicInterface {
         }, {blocksBehind: 3, expireSeconds: 30});
     }
 }
-(async function (){
 
-    let keys = [
-        "5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5",
-        "5KLqT1UFxVnKRWkjvhFur4sECrPhciuUqsYRihc1p9rxhXQMZBg",
-        "5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7",
-        "5KNm1BgaopP9n5NqJDo9rbr49zJFWJTMJheLoLM5b7gjdhqAwCx",
-        "5KE2UNPCZX5QepKcLpLXVCLdAw7dBfJFJnuCHhXUf61hPRMtUZg",
-        "5KaqYiQzKsXXXxVvrG8Q3ECZdQAj2hNcvCgGEubRvvq7CU3LySK",
-        "5KFyaxQW8L6uXFB6wSgC44EsAbzC7ideyhhQ68tiYfdKQp69xKo"
-    ];
+// (async function (){
+//
+//     let keys = [
+//         "5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5",
+//         "5KLqT1UFxVnKRWkjvhFur4sECrPhciuUqsYRihc1p9rxhXQMZBg",
+//         "5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7",
+//         "5KNm1BgaopP9n5NqJDo9rbr49zJFWJTMJheLoLM5b7gjdhqAwCx",
+//         "5KE2UNPCZX5QepKcLpLXVCLdAw7dBfJFJnuCHhXUf61hPRMtUZg",
+//         "5KaqYiQzKsXXXxVvrG8Q3ECZdQAj2hNcvCgGEubRvvq7CU3LySK",
+//         "5KFyaxQW8L6uXFB6wSgC44EsAbzC7ideyhhQ68tiYfdKQp69xKo"
+//     ];
+//
+//     let userAccounts = [
+//         "useraaaaaaaa",
+//         "useraaaaaaab",
+//         "useraaaaaaac",
+//         "useraaaaaaad",
+//         "useraaaaaaae",
+//         "useraaaaaaaf",
+//         "useraaaaaaag"
+//     ];
+//
+//     let contractName = "useraaaaaaag";
+//     let interface = new MusicInterface(keys, contractName, "http://64.38.145.139:8888");
+//     // try {
+//     //     await interface.initializeMockData();
+//     // } catch (e) {
+//     //     console.log("Error initializing mock data: ", e);
+//     // }
+//
+//     let artist = await interface.getArtist(userAccounts[3]);
+//     let album_songs = await interface.getAllSongsFromArtist(userAccounts[3]);
+//
+//     console.log(artist);
+//     console.log(album_songs);
+// })();
 
-    let userAccounts = [
-        "useraaaaaaaa",
-        "useraaaaaaab",
-        "useraaaaaaac",
-        "useraaaaaaad",
-        "useraaaaaaae",
-        "useraaaaaaaf",
-        "useraaaaaaag"
-    ];
+let contractName = "useraaaaaaag";
+let interface = new MusicInterface(keys, contractName, "http://64.38.145.139:8888");
 
-    let contractName = "useraaaaaaag";
-    let interface = new MusicInterface(keys, contractName, "http://64.38.145.139:8888");
-    // try {
-    //     await interface.initializeMockData();
-    // } catch (e) {
-    //     console.log("Error initializing mock data: ", e);
-    // }
-
-    let artist = await interface.getArtist(userAccounts[3]);
-    let album_songs = await interface.getAllSongsFromArtist(userAccounts[3]);
-
-    console.log(artist);
-    console.log(album_songs);
-})();
+module.exports = interface;
